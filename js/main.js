cb7eac3d34402409a82bff42957df52d62154e38
Rekenium <jrequena9@gmail.com>
2018-02-16 14:06:26 +0100

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,8 @@
+window.onload = function() {
+	if (window.matchMedia("(min-width: 1024px)").matches) {
+
+		var a = document.getElementsByClassName("container");
+		a[0].style.display = "none";
+
+	}
+}
